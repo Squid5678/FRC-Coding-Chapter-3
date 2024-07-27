53fee375aa42c63b50a6b1d673197872902d4cb2
Aalind Tyagi <42896179+Squid5678@users.noreply.github.com>
2024-07-27 19:27:21 -0400

    push 3.5

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -9,7 +9,9 @@ import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.controls.Follower;
 import com.ctre.phoenix6.hardware.TalonFX;
 
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
@@ -23,13 +25,32 @@ public class Robot extends TimedRobot {
   private Command m_autonomousCommand;
 
   private RobotContainer m_robotContainer;
-  
-  private TalonFX primaryMotor = new TalonFX(1);
-  private TalonFX followerMotor1 = new TalonFX(2);
-  private TalonFX followerMotor2 = new TalonFX(3);
 
-  private TalonFX leftMotor = new TalonFX(4);
-  private TalonFX rightMotor = new TalonFX(5);
+  /* Instantiate the 4 Drivetrain Motors */
+  private TalonFX leftSidePrimary = new TalonFX(0);
+  private TalonFX leftSideFollower = new TalonFX(1);
+  private TalonFX rightSidePrimary = new TalonFX(2);
+  private TalonFX rightSideFollower = new TalonFX(3);
+
+  /* Instantiate the Shooter Motor */
+  private TalonFX shooterMotor = new TalonFX(4);
+
+  /* Instantiate the Elevator Motor */
+  private TalonFX elevatorMotor = new TalonFX(5);
+
+  /* Instantiate the WPILib Timer */
+  private Timer timer = new Timer();
+
+  /* Instantiate our XBOX Controller */
+  private final Joystick xboxController = new Joystick(0);
+  
+
+  /*For toggle */
+  private boolean buttonIsPressed;
+
+  /*For elevator */
+  private double elevatorTime;
+  private int direction = -1; //-1 by default so that it flips to +1 on first toggle
 
   /**
    * This function is run when the robot is first started up and should be used for any
@@ -41,35 +62,53 @@ public class Robot extends TimedRobot {
     // autonomous chooser on the dashboard.
     m_robotContainer = new RobotContainer(); 
 
-    /* Reset the motor to its factory default. */
-    primaryMotor.getConfigurator().apply(new TalonFXConfiguration());
-    followerMotor1.getConfigurator().apply(new TalonFXConfiguration());
-    followerMotor2.getConfigurator().apply(new TalonFXConfiguration());
-    leftMotor.getConfigurator().apply(new TalonFXConfiguration());
-    rightMotor.getConfigurator().apply(new TalonFXConfiguration());
+    /* Begin by setting all of the motors to their factory defaults. */
+    leftSidePrimary.getConfigurator().apply(new TalonFXConfiguration());
+    leftSideFollower.getConfigurator().apply(new TalonFXConfiguration());
+    rightSidePrimary.getConfigurator().apply(new TalonFXConfiguration());
+    rightSideFollower.getConfigurator().apply(new TalonFXConfiguration());
+    shooterMotor.getConfigurator().apply(new TalonFXConfiguration());
+    elevatorMotor.getConfigurator().apply(new TalonFXConfiguration());
 
-    /* Configure the current of the motor */
+    /* Next, set the necessary inverts and tell the follower motors to follow. */
+    rightSidePrimary.setInverted(true);
+    rightSideFollower.setInverted(true);
+    leftSideFollower.setControl(new Follower(0, false));
+    rightSideFollower.setControl(new Follower(2, false));
+
+    /* Finally, call the custom method that sets the current limits. */
+    configureMotorCurrentLimits();
+  }
+
+  /**
+   * Method used to configure the motor current limits.
+   */
+  public void configureMotorCurrentLimits() {
+    /* Declare the current configuration. */
     var currentConfiguration = new CurrentLimitsConfigs();
+
+    /* Refresh all the motor's existing current configuration with our new motor configuration.
+     * This essentially tells the motor to expect a new current configuration so we don't
+     * end up in a fight with other timing loops when initializing the motor's other configs.
+    */
+    leftSidePrimary.getConfigurator().refresh(currentConfiguration);
+    leftSideFollower.getConfigurator().refresh(currentConfiguration);
+    rightSidePrimary.getConfigurator().refresh(currentConfiguration);
+    rightSideFollower.getConfigurator().refresh(currentConfiguration);
+    shooterMotor.getConfigurator().refresh(currentConfiguration);
+    elevatorMotor.getConfigurator().refresh(currentConfiguration);
+
+    /* Setup the Current Configuration */
     currentConfiguration.StatorCurrentLimit = 80;
     currentConfiguration.StatorCurrentLimitEnable = true;
 
-    primaryMotor.getConfigurator().refresh(currentConfiguration);
-    followerMotor1.getConfigurator().refresh(currentConfiguration);
-    followerMotor2.getConfigurator().refresh(currentConfiguration);
-    leftMotor.getConfigurator().refresh(currentConfiguration);
-    rightMotor.getConfigurator().refresh(currentConfiguration);
-
-    primaryMotor.getConfigurator().apply(currentConfiguration);
-    followerMotor1.getConfigurator().apply(currentConfiguration);
-    followerMotor2.getConfigurator().apply(currentConfiguration);
-    leftMotor.getConfigurator().apply(currentConfiguration);
-    rightMotor.getConfigurator().apply(currentConfiguration);
-
-    followerMotor1.setControl(new Follower(1, false));
-    followerMotor1.setControl(new Follower(1, false));
-
-    rightMotor.setInverted(true);
-    
+    /* Finally, apply the current configuration. */
+    leftSidePrimary.getConfigurator().apply(currentConfiguration);
+    leftSideFollower.getConfigurator().apply(currentConfiguration);
+    rightSidePrimary.getConfigurator().apply(currentConfiguration);
+    rightSideFollower.getConfigurator().apply(currentConfiguration);
+    shooterMotor.getConfigurator().apply(currentConfiguration);
+    elevatorMotor.getConfigurator().apply(currentConfiguration);
   }
 
   /**
@@ -119,15 +158,58 @@ public class Robot extends TimedRobot {
     if (m_autonomousCommand != null) {
       m_autonomousCommand.cancel();
     }
+
+    /* Reset the timer before teleop starts. */
+    timer.stop();
+    timer.reset();
   }
 
   /** This function is called periodically during operator control. */
   @Override
   public void teleopPeriodic() {
-    /* Set the motor's percent output. */
-    primaryMotor.set(0.5);
-    leftMotor.set(0.5);
-    rightMotor.set(0.5);
+    /* TANK DRIVE CONTROL */
+    leftSidePrimary.set(xboxController.getRawAxis(1)); //should be the y-axis of the left joystick
+    rightSidePrimary.set(xboxController.getRawAxis(2)); //should be the y-axis of the right joystick
+
+    /* SHOOTER MOTOR CONTROL */
+    /* NOTE: Change button ID to whatever you want (my controller's A button = 1) */
+    if (xboxController.getRawButton(1)) {
+      shooterMotor.set(0.5);
+    } else {
+      shooterMotor.set(0);
+    }
+
+    
+
+
+    /* TOGGLE HANDLING */
+
+    /* NOTE: Change button ID to whatever you want (my controller's B button = 2) */
+
+    /*CASE 1: Pressing button */
+    if (xboxController.getRawButton(2) && buttonIsPressed == false) {
+      buttonIsPressed = true;
+      
+      direction = -1*direction;
+      timer.reset();
+      timer.start();
+    }
+    /* CASE 2: Releasing button */
+    else if (!xboxController.getRawButton(2) && buttonIsPressed == true){
+      buttonIsPressed = false;
+    }
+
+    /* ELEVATOR MOTOR CONTROL */    
+    if (timer.get() > 5){
+      elevatorMotor.set(0);
+      timer.stop();
+    }
+    else if (timer.get() >0.01) {
+      elevatorMotor.set(0.5 * direction);
+    }
+
+   
+    
   }
 
   @Override
